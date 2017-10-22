3c9183f074ea73526cb99db2aed59e8aa462a6e6
hpkent <hpkent@gmail.com>
2017-10-21 21:34:23 -0400

    working version from Hackathon

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,44 +1,44 @@
 class GroupsController < ApplicationController
 
-  #students_groups
-  def all_students_in_group
+  #clients_groups
+  def all_clients_in_group
     @group = Group.find(params[:id])
     @group_id = @group.id
     @group_name = @group.name
-    @students_groups = StudentsGroup.new
+    @clients_groups = StudentsGroup.new
     @look_up_group = Group.where(name:@group_name)
     @look_up_group_id = @look_up_group.first.id
-    @students_in_groups = Student.select('students.id, students.first_name, students.last_name, students_groups.id AS students_groups_id, groups.name AS group_name, category_types.name AS category_name').joins('
-    INNER JOIN students_groups ON students_groups.student_id = students.id 
-    INNER JOIN groups ON students_groups.group_id = groups.id
-    LEFT OUTER JOIN category_types ON category_types.id = students.category_type_id' 
-    ).where(" 
+    @clients_in_groups = Student.select('clients.id, clients.first_name, clients.last_name, clients_groups.id AS clients_groups_id, groups.name AS group_name, category_types.name AS category_name').joins('
+    INNER JOIN clients_groups ON clients_groups.client_id = clients.id
+    INNER JOIN groups ON clients_groups.group_id = groups.id
+    LEFT OUTER JOIN category_types ON category_types.id = clients.category_type_id'
+    ).where("
     groups.id =?", @look_up_group_id).order(:first_name)
-    @students = Student.active_student.order(:category_type_id)
+    @clients = Student.active_client.order(:category_type_id)
   end
 
-  def update_students_in_group
-    @students_group = StudentsGroup.where(group_id:params[:students_group][:group_id],student_id:params[:students_group][:student_id]).first_or_initialize
+  def update_clients_in_group
+    @clients_group = StudentsGroup.where(group_id:params[:clients_group][:group_id],client_id:params[:clients_group][:client_id]).first_or_initialize
     respond_to do |format|
-      if (@students_group.update(students_group_params))
-      @group_id = @students_group.group_id
-        format.html { redirect_to("/groups/#{@group_id}/all_students_in_group") }
-        format.json 
-        format.js   
+      if (@clients_group.update(clients_group_params))
+      @group_id = @clients_group.group_id
+        format.html { redirect_to("/groups/#{@group_id}/all_clients_in_group") }
+        format.json
+        format.js
       else
-        format.html { redirect_to("/groups/#{@group_id}/all_students_in_group") }
+        format.html { redirect_to("/groups/#{@group_id}/all_clients_in_group") }
       end
     end
   end
 
-  def delete_students_groups
-    students_groups = StudentsGroup.find(params[:students_groups_id])
-    @group_id = students_groups.group_id
-    students_groups.destroy
+  def delete_clients_groups
+    clients_groups = StudentsGroup.find(params[:clients_groups_id])
+    @group_id = clients_groups.group_id
+    clients_groups.destroy
     respond_to do |format|
-      format.html { redirect_to "/groups/#{@group_id}/all_students_in_group"}
-      format.json 
-      format.js  
+      format.html { redirect_to "/groups/#{@group_id}/all_clients_in_group"}
+      format.json
+      format.js
     end
   end
 
@@ -54,7 +54,7 @@ class GroupsController < ApplicationController
   def edit
     @group = Group.find(params[:id])
   end
- 
+
   def update
     @group = Group.where(id:params[:id]).first_or_initialize
     if @group.update(group_params)
@@ -75,10 +75,10 @@ class GroupsController < ApplicationController
       params.require(:group).permit(:name, :description)
     end
 
-    def students_group_params
-      params.require(:students_group).permit(:group_id, :student_id)
+    def clients_group_params
+      params.require(:clients_group).permit(:group_id, :client_id)
     end
-end  
+end
 
 
 
